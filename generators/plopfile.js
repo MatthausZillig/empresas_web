9c1dd7ccad97283c9dafe28a1c499240cd663a36
Matthaus <matthauszigillig@hotmail.com>
2020-09-05 19:37:00 -0300

    chore: add others templates and commands in generators

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -12,17 +12,85 @@
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/index.jsx',
-        templateFile: 'templates/Component.jsx.hbs',
+        templateFile: 'templates/component/Component.jsx.hbs',
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/styles.ts',
-        templateFile: 'templates/styles.js.hbs',
+        templateFile: 'templates/component/styles.js.hbs',
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/test.spec.js',
-        templateFile: 'templates/test.spec.js.hbs',
+        templateFile: 'templates/component/test.spec.js.hbs',
+      },
+    ],
+  });
+  plop.setGenerator('page', {
+    description: 'Create a page',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your page name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/pages/{{dashCase name}}/{{dashCase name}}.jsx',
+        templateFile: 'templates/component/Component.jsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/pages/{{dashCase name}}/styles.js',
+        templateFile: 'templates/component/styles.js.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/pages/{{dashCase name}}/{{dashCase name}}.spec.js',
+        templateFile: 'templates/component/test.spec.js.hbs',
+      },
+    ],
+  });
+
+  plop.setGenerator('hook', {
+    description: 'Create a hook',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your hook name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/hooks/use-{{dashCase name}}/use-{{dashCase name}}.js',
+        templateFile: 'templates/hook/hook.js.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/hooks/use-{{dashCase name}}/use-{{dashCase name}}.spec.js',
+        templateFile: 'templates/hook/test.spec.js.hbs',
+      },
+    ],
+  });
+
+  plop.setGenerator('context', {
+    description: 'Create a context',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your context name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/contexts/{{dashCase name}}.js',
+        templateFile: 'templates/context/context.js.hbs',
       },
     ],
   });
